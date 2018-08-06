a3687b5138eff1b5762bc53710ed6d86fdae116a
thepante <thepante@users.noreply.github.com>
2018-08-06 16:59:02 -0300

    Add files via upload

---
+++ .config/deb.lua
@@ -0,0 +1,6 @@
+debug_print("--------------------------------------------");
+debug_print("Window Name:       " .. get_window_name());
+debug_print("Application name:  " .. get_application_name())
+debug_print("XID:               " .. get_window_xid())
+debug_print("Window Class:      " .. get_window_class())
+debug_print()
