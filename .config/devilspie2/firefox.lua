797b1305c911cfe1d7fca2f9378d1e35c6c9b8a9
thepante <panterua@hotmail.com>
2019-01-18 15:54:58 -0300

    updated

--- .config/devilspie2/firefox.lua
+++ .config/devilspie2/firefox.lua
@@ -1,6 +1,16 @@
 if (get_application_name()=="Firefox") then
-	if (get_window_role()=="toolbox") then
-		set_window_position(400, 240);
-	else set_window_position(30, 50); end
+	if (get_window_class()=="Nightly") then
+			if (get_window_class()>="Devtools") then
+			set_window_geometry(400, 240, 1100, 700);
+			-- set_window_position(800, 240);
+			end
+			else
+			set_window_geometry(15, 50, 1170, 990);
+	end
+	if (get_window_class()=="Firefox") then
+		set_window_geometry(30, 50, 1600, 970); 
+	end
+
+	--else set_window_position(30, 50); end
 end
 
