1819af563bb21a096e7a037fafa1f742fcf3b8e1
thepante <panterua@hotmail.com>
2019-01-18 23:10:43 -0300

    updates

--- .config/devilspie2/terminal.lua
+++
@@ -1,3 +0,0 @@
-if (get_window_class()=="Gnome-terminal") then
-	set_window_position(1190, 24);
-end
--- .config/devilspie2/ubuntu-software.lua
+++
@@ -1,3 +0,0 @@
-if (get_application_name()=="Ubuntu Software") then
-	set_window_geometry(100, 150, 800, 600);
-end
