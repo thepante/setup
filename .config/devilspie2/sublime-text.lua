b1742ffe4dacd2f50ae56f74f234a0e977b487a8
thepante <panterua@hotmail.com>
2019-02-12 23:34:14 -0300

    updates

--- .config/devilspie2/sublime-text.lua
+++ .config/devilspie2/sublime-text.lua
@@ -1,3 +1,5 @@
-if (get_application_name()=="Sublime Text") then
-	set_window_geometry(30, 25, 915, 980);
+if (get_window_class()=="Sublime_text") then
+  if (get_window_type()=="WINDOW_TYPE_NORMAL") then
+	  set_window_geometry(30, 25, 915, 980);
+  end
 end
