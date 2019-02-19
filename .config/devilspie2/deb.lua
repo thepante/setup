8f43d73789c81c3559e669d6352a64e1d0148f76
thepante <panterua@hotmail.com>
2019-02-19 10:01:00 -0300

    updated

--- .config/devilspie2/deb.lua
+++ .config/devilspie2/deb.lua
@@ -1,4 +1,6 @@
 debug_print("--------------------------------------------");
+x, y, width, height = get_window_geometry();
+
 debug_print("XID:               " .. get_window_xid())
 debug_print("Window Name:       " .. get_window_name());
 debug_print("Application name:  " .. get_application_name())
@@ -6,6 +8,6 @@ debug_print("Window Class:      " .. get_window_class())
 debug_print("Window type:       " .. get_window_type())
 debug_print("Window role:       " .. get_window_role())
 debug_print("Class inst. name:  " .. get_class_instance_name())
-debug_print("Window property    " .. get_window_property());
+debug_print("Window geometry    " .. "X: "..x..", Y: "..y..", width: "..width..", height: "..height);
 
 debug_print()
--- .config/devilspie2/firefox.lua
+++ .config/devilspie2/firefox.lua
@@ -1,13 +1,13 @@
 if (get_application_name()=="Firefox") then
 	if (get_window_class()=="Nightly") then
 			if (get_window_role()=="toolbox") then
-			set_window_geometry(500, 300, 1100, 600);
+			set_window_geometry(946, -32, 974, 580);
 			end
 			if (get_window_role()=="browser") then
 			set_window_geometry(15, 50, 1165, 990); 
 			end
 			if (get_window_role()=="webconsole") then
-			set_window_geometry(400, 240, 1000, 400);
+			set_window_geometry(0, -41, 1000, 284);
 			end
 	end
 	if (get_window_class()=="Firefox") then
