b3184ae48208cde1299319d623b2e668e83bc9bd
pante <panterua@hotmail.com>
2020-07-05 21:31:52 -0300

    added cam setup

---
+++ startcam.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apps=("droidcam" "obs")
+
+for app in ${apps[@]}; do
+	if ! pgrep -x $app > /dev/null
+	then
+		$app &
+	fi
+done
+
+# Start SWS
+if ! pgrep -x "SoundWireServer" > /dev/null
+then
+  /opt/SoundWireServer/SoundWireServer &
+fi
+
+# Load & set droidcam_audio
+dainput=$(pacmd list-sources | grep droidcam_audio)
+if [ -z "$dainput" ]
+then
+  pacmd load-module module-alsa-source device=hw:Loopback,1,0 source_properties=device.description=droidcam_audio
+fi
+pacmd set-default-source "alsa_input.hw_Loopback_1_0"
+
+# Open SoundWire client and droidcamx on the phone
+adb shell monkey -p com.georgie.SoundWireFree -v 1
+adb shell monkey -p com.dev47apps.droidcamx -v 1 &
+
+xdg-open "https://www.youtube.com/watch?v=7w1Qo1awwI4" &
